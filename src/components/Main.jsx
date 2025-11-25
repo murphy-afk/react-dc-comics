ea20af51d723099031f63f5108f5ee57b259cf50
murphy-afk <bardarenoemi@gmail.com>
2025-11-25 15:51:33 +0100

    feat: [Main] links section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,10 @@
 import "./Main.css";
+import DigitalComics from "../assets/img/buy-comics-digital-comics.png";
+import Merchandise from "../assets/img/buy-comics-merchandise.png";
+import ShopLocation from "../assets/img/buy-comics-shop-locator.png";
+import Subscriptions from "../assets/img/buy-comics-subscriptions.png";
+import Visa from "../assets/img/buy-dc-power-visa.svg";
+
 
 function Content() {
   return (
@@ -8,11 +14,42 @@ function Content() {
   )
 }
 
+function Links() {
+  return (
+    // nav?
+    <div className="links-container">
+
+      <nav className="links">
+        <a href="#">
+          <img src={DigitalComics} alt="digital comics" />
+          DIGITAL COMICS
+        </a>
+        <a href="#">
+          <img src={Merchandise} alt="merchandise" />
+          MERCHANDISE
+        </a>
+        <a href="#">
+          <img src={ShopLocation} alt="shop location" />
+          COMIC SHOP LOCATION
+        </a>
+        <a href="#">
+          <img src={Subscriptions} alt="subscriptions" />
+          SUBSCRIPTION
+        </a>
+        <a href="#">
+          <img src={Visa} alt="dc power visa" />
+          DC POWER VISA
+        </a>
+      </nav>
+    </div>
+  )
+}
+
 export default function Main() {
   return (
     <main className="main">
       <Content />
-      {/* info bar (links) */}
+      <Links />
     </main>
   )
 }
